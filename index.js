7c3ca50fe376e7462835ff67a9c2f06ba5013150
nmeza-cudesi <76980692+nmeza-cudesi@users.noreply.github.com>
2022-07-13 00:26:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express');
+
+const path = require('path');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.SERVER_PORT || 4000);
+
+//permisos
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", process.env.URI_BACKEND);
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        'X-Requested-With, Authorization, Content-Type, Content-Length'
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+// Routes
+app.use(require('./src/routes/image.routes'));
+
+// Static files
+app.use("/upload", express.static(path.join(__dirname, '/src/public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
---
+++ src/routes/image.routes.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const { Router } = require('express');
+const router = new Router();
+
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public'),
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+const uploadImage = multer({
+    storage,
+    limits: { fileSize: 1000000 }
+}).single('image');
+
+router.post('/images/upload', (req, res) => {
+    uploadImage(req, res, (err) => {
+        if (err) {
+            err.message = 'The file is so heavy for my service';
+            return res.send(err);
+        }
+        console.log(req.file);
+        res.send(process.env.URI_FILE + "/upload/" + req.file.originalname);
+    });
+});
+router.get('/', (req, res) => {
+    res.send({"response":"Fuciona Correctamente"});
+});
+
+
+module.exports = router;
